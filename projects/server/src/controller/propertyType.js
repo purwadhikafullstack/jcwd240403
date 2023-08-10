82d26757fd8f0eea329a5107fc98a49d0743f377
prizkideanne <prizkideanne@gmail.com>
2023-08-11 06:10:58 +0700

    add controller property type

--- projects/server/src/controller/propertyType.js
+++ projects/server/src/controller/propertyType.js
@@ -1,5 +1,6 @@
 const { compareSync } = require("bcryptjs");
 const db = require("../models");
+const { where } = require("sequelize");
 
 module.exports = {
   async getAllPropType(req, res) {
@@ -36,6 +37,97 @@ module.exports = {
     } catch (error) {
       console.log("errAddPropType", error);
       res.status(500).send({ message: "Something wrong on server", error });
+    }
+  },
+
+  async getOneMyPropType(req, res) {
+    try {
+      const id = Number(req.params.id);
+      const userId = req.user.id;
+      const result = await db.Property_type.findOne({
+        where: { id: id, user_id: userId },
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+      });
+
+      if (!result) {
+        return res.status(400).send({
+          message: "Property type not found",
+        });
+      }
+      res.status(200).send({
+        message: "Success get property type",
+        data: result,
+      });
+    } catch (error) {
+      console.log("myOnePropType", error);
+      res.status(500).send({
+        message: "Something wrong on server",
+        error,
+      });
+    }
+  },
+
+  async editMyPropType(req, res) {
+    try {
+      const { id } = req.params;
+      const userId = req.user.id;
+      const { newName } = req.body;
+      const propType = await db.Property_type.findOne({
+        where: { id: id, user_id: userId },
+      });
+
+      if (!propType) {
+        return res.status(400).send({
+          message: "Property type not found",
+        });
+      }
+
+      await db.Property_type.update({ name: newName }, { where: { id: id } });
+
+      const updatedPropType = await db.Property_type.findOne({
+        where: { id: id, user_id: userId },
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+      });
+
+      res.status(201).send({
+        message: "Success update property type name",
+        data: updatedPropType,
+      });
+    } catch (error) {
+      console.log("editproptype", error);
+      res.status(500).send({
+        message: "Something wrong on server",
+        error,
+      });
+    }
+  },
+
+  async deleteMyPropertyType(req, res) {
+    try {
+      const id = Number(req.params.id);
+      const userId = req.user.id;
+
+      const propType = await db.Property_type.findOne({
+        where: { id: id, user_id: userId },
+      });
+
+      if (!propType) {
+        return res.status(400).send({
+          message: "Property type not found",
+        });
+      }
+
+      await propType.destroy();
+
+      return res.status(200).send({
+        message: "Property type successfuly deleted",
+      });
+    } catch (error) {
+      console.log("deleteproptype", error);
+      res.status(500).send({
+        message: "Something wrong on server",
+        error,
+      });
     }
   },
 };
--- projects/server/src/routes/propertyType.js
+++ projects/server/src/routes/propertyType.js
@@ -12,4 +12,24 @@ router.post(
   propTypeController.addPropertyType
 );
 
+router.get(
+  "/:id",
+  authMiddleware.verifyAccessToken,
+  authMiddleware.verifyTenant,
+  propTypeController.getOneMyPropType
+);
+
+router.patch(
+  "/edit/:id",
+  authMiddleware.verifyAccessToken,
+  authMiddleware.verifyTenant,
+  propTypeController.editMyPropType
+);
+
+router.delete(
+  "/delete/:id",
+  authMiddleware.verifyAccessToken,
+  authMiddleware.verifyTenant,
+  propTypeController.deleteMyPropertyType
+);
 module.exports = router;
